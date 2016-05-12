4dbabeccf8548b7deaf1fcbebdc7c6f65c9f893c
Chris Nuernberger <chris@thinktopic.com>
2016-05-12 13:34:10 -0600

    Adding in credentials for leiningen

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,1 @@
-{:user {:dependencies [[pjstadig/humane-test-output "0.7.0"]]
-        :injections [(require 'pjstadig.humane-test-output)
-                     (pjstadig.humane-test-output/activate!)]
-        :plugins [[cider/cider-nrepl "0.8.2"]
-                  [refactor-nrepl "0.2.2"]
-                  [jonase/eastwood "0.2.1"]
-                  [lein-kibit "0.0.8"]] }}
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
